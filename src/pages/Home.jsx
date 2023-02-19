95c6237201c0f29f23915c3cee0a5a9ac475c883
marnathali <nathalianzola@gmail.com>
2023-02-19 16:15:41 -0500

    NEW: header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>React Shop</h1>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            Not Found 404
+            <img src='./images/img-notfound.jpg'></img>
+        </div>
+    );
+};
+
+export default NotFound;
